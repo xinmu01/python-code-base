cd957cc527e65c2ec96bf8a3929491bf41cceba8
Xin Mu <xinmu525@gmail.com>
2019-02-18 16:37:10 -0600

    The test file for python linked list class -- First commit

---
+++ LinkedList/LinkedListRun.py
@@ -0,0 +1,58 @@
+import LinkedListClass as LL 
+
+if __name__ == "__main__":   
+
+    
+    #Generate Linked List
+    linkList = LL.SingleLinkedList(1)
+    linkList.connect(2)
+    linkList.connect(3)
+    linkList.connect(4)
+    linkList.connect(1)
+    linkList.connect(2)
+    #Check Linked List
+    print (linkList.length)
+    print (linkList.checkExist(3))
+    print (linkList.checkExist(6))   
+    linkList.printList()
+    #Reverse Linked List
+    linkList.reverseLinkedList()    
+    linkList.printList()
+    #Reverse it back
+    linkList.reverseLinkedList()
+    linkList.printList()
+    #Insert one noed
+    linkList.insertNodeLinkedList(5,5)
+    linkList.printList()
+    print(linkList.length)
+    print(linkList.isEmpty)
+    #Delete one node by index
+    linkList.deleteNodeLinkedListPosition(100)
+    linkList.printList()
+    linkList.deleteNodeLinkedListPosition(3)
+    linkList.printList()
+    print(linkList.length)
+    print(linkList.isEmpty)
+    #Delete one node by value
+    linkList.deleteNodeLinkedListValue(100)
+    linkList.printList()
+    linkList.deleteNodeLinkedListValue(2)
+    linkList.printList()
+    linkList.deleteNodeLinkedListValue(4)
+    linkList.printList()
+    print (linkList.checkExist(3))
+    print (linkList.length)
+    linkList.deleteNodeLinkedListValue(1)
+    linkList.printList()
+    linkList.deleteNodeLinkedListValue(3)
+    linkList.printList()
+    linkList.deleteNodeLinkedListValue(5)
+    linkList.printList()
+    print (linkList.length)
+    print (linkList.isEmpty)
+    #Insert one node again
+    linkList.insertNodeLinkedList(1,1000)
+    linkList.printList()
+    print(linkList.length)
+    print(linkList.isEmpty)
+
