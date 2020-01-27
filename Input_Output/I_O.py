612df102fed55afe327c16f8fe74b889018d543a
Xin Mu <xinmu525@gmail.com>
2020-01-27 11:22:48 -0600

    Reconsturcture the folders.

--- Input_Output/I_O.py
+++ Input_Output/I_O.py
@@ -1,21 +1,11 @@
 f = open("test.txt",'r') 
-g=open("out.txt",'w')
+g = open("out.txt",'w')
 
-#print(f.read())
-#print(f.read()[3])
-#print(type(f.read()))
-#print(next(f.readlines()))
+for line in f.readlines():
+    print(line.strip())
+    g.write(line)
 
-#for line in f.readlines():
-#    print(line)
-    #print(1)
-#    g.write(line)
-
-print(f.readline())    
-print(f.readline())  
-print(f.readline())  
-print(f.readline()) 
-print(f.readline())   
+   
 f.close()
 g.close()
 
